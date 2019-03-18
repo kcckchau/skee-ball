c5c7396c827c24b44e89d227362c20eb6bc51049
KCCK <kcckstudio@gmail.com>
2019-03-19 01:13:18 +0800

    [JIRA-1] Add initial scenes and scripts

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void GotoLeaderboard()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void GotoGameLevel()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+}
