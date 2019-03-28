c71e21ceca194b6b2d5f4ca6e80a4f73adee0ba4
KCCK <kcckstudio@gmail.com>
2019-03-28 15:34:11 +0800

    Support Desktop Platform Control

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -39,18 +39,78 @@ public class InputController : MonoBehaviour {
 		timeToMax = 1.5f;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
+	void DesktopInputControl()
+	{
+		Debug.Log("DesktopInputControl");
+		switch (state)
 		{
-			SceneController.GotoMainScene();
+			case State.IDLE:
+			{
+				break;
+			}
+			case State.READY:
+			{
+				if (Input.GetMouseButtonDown(0) )
+				{
+					state = State.POWER_IN_PROGRESS;
+					elapsedTime = 0;
+				}
+				break;
+			}
+			case State.POWER_IN_PROGRESS:
+			{
+				if (Input.GetMouseButton(0))
+				{
+
+					if (power < 0f)
+					{
+						elapsedTime = 0;
+						direction = 1;
+					}
+					else if (power > 1.0f)
+					{
+						elapsedTime = 0;
+						direction = -1;
+					}
+
+					//power += 1.0f * (Time.deltaTime / timeToMax + power * 0.1f) * direction;
+					elapsedTime += Time.deltaTime;
+					if (direction > 0)
+						power = 0.5f * acceleration * elapsedTime * elapsedTime;
+					else
+						power = 1 - (0.5f * acceleration * elapsedTime * elapsedTime);
+					GameController.instance.UpdatePowerBar(power);
+
+					Camera cam = Camera.main;
+					Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+					RaycastHit rayCast;
+					if(Physics.Raycast(ray.origin, ray.direction, out rayCast, 100))
+					{
+						float newX = rayCast.point.x;
+						GameController.instance.UpdateBallPositionX(newX);
+					}
+				}
+				else if (Input.GetMouseButtonUp(0))
+				{
+					 GameController.instance.Shoot(power);
+					 power = 0;
+					 elapsedTime = 0;
+					 direction = 1;
+					 state = State.POWER_ENDED;
+				}
+				break;
+			}
+			case State.POWER_ENDED:
+			{
+				break;
+			}
 		}
 
-		if (GameController.instance.state == GameController.State.INPUT_NAME ||
-		GameController.instance.state == GameController.State.COUNT_DOWN ||
-		GameController.instance.state == GameController.State.GAME_OVER)
-			return;
+	}
 
+	void MobileInputControl()
+	{
+		Debug.Log("MobileInputControl");
 		Touch[] touches = Input.touches;
 		Touch touch;
 
@@ -136,6 +196,32 @@ public class InputController : MonoBehaviour {
 			{
 				break;
 			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			SceneController.GotoMainScene();
+		}
+
+		GameController.State gameState = GameController.instance.state;
+
+		if (gameState == GameController.State.INPUT_NAME ||
+		gameState == GameController.State.COUNT_DOWN ||
+		gameState == GameController.State.GAME_OVER)
+			return;
+
+		if (Application.platform == RuntimePlatform.Android ||
+			Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			MobileInputControl();
+		}
+		else
+		{
+			// assume anything else is Desktop at the moment
+			DesktopInputControl();
 		}
 	}
 
